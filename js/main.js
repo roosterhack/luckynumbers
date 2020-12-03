49a311ad3be37c43f52747170932e30539493d22
eddie chung <eddie.chung@moonfare.com>
2020-12-03 09:34:41 +0100

    fix logic

--- js/main.js
+++ js/main.js
@@ -4,50 +4,53 @@ var clearNumsButton = document.getElementById('clearNums');
 
 //get all input numbers
 
-generateButton.addEventListener('click', function () {
-  var error = false;
-  var n1 = document.querySelector('#n1').value;
-  var n2 = document.querySelector('#n2').value;
-  var n3 = document.querySelector('#n3').value;
-  var n4 = document.querySelector('#n4').value;
-  var n5 = document.querySelector('#n5').value;
-  var n6 = document.querySelector('#n6').value;
-  var n7 = document.querySelector('#n7').value;
-  var inputNums = [...n1, ...n2, ...n3, ...n4, ...n5, ...n6, ...n7];
-  var lastResults = inputNums.map((n) => {
-    if (n) {
-      return Number(n);
+generateButton.addEventListener(
+  'click',
+  function () {
+    var n1 = document.querySelector('#n1').value;
+    var n2 = document.querySelector('#n2').value;
+    var n3 = document.querySelector('#n3').value;
+    var n4 = document.querySelector('#n4').value;
+    var n5 = document.querySelector('#n5').value;
+    var n6 = document.querySelector('#n6').value;
+    var n7 = document.querySelector('#n7').value;
+    var inputNums = [n1, n2, n3, n4, n5, n6, n7];
+
+    var lastResults = inputNums.map((n) => {
+      if (n) {
+        return Number(n);
+      }
+      return null;
+    });
+
+    //get random numbers
+    // if (!lastResults.includes(null)) {
+    var numbers = [];
+    console.log(lastResults);
+    while (numbers.length < 7) {
+      // const takeOutNum = Math.floor(Math.random() * 49) + 1;
+
+      var random = Math.floor(Math.random() * 49) + 1;
+
+      if (numbers.indexOf(random) === -1 && !lastResults.includes(random)) {
+        numbers.push(random);
+      }
     }
-    error = true;
-    return;
-  });
 
-  //get random numbers
+    //add numbers to history
+    var history = [];
+    var historyDiv = document.getElementById('history');
+    var para = document.createElement('li');
+    history.push(numbers.join(' - '));
 
-  var numbers = [];
-
-  while (numbers.length < 7 && lastResults) {
-    // const takeOutNum = Math.floor(Math.random() * 49) + 1;
-
-    var random = Math.floor(Math.random() * 49) + 1;
-
-    if (numbers.indexOf(random) == -1 && !lastResults.includes(random)) {
-      numbers.push(random);
+    for (var k in history) {
+      var node = document.createTextNode(history[k]);
+      para.appendChild(node);
+      historyDiv.appendChild(para);
     }
   }
-
-  //add numbers to history
-  var history = [];
-  var historyDiv = document.getElementById('history');
-  var para = document.createElement('li');
-  history.push(numbers.join(' - '));
-
-  for (var k in history) {
-    var node = document.createTextNode(history[k]);
-    para.appendChild(node);
-    historyDiv.appendChild(para);
-  }
-});
+  // }
+);
 
 clearNumsButton.addEventListener('click', () => {
   n1.value = '';
